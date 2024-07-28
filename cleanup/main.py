d7d9df0e72464842e2ba8bba2f1df41ceb04d9a6
Bogdan Yakupov <yourinfinityuser@gmail.com>
2024-07-28 16:54:31 +0000

    changed naming for cleanup executable file

---
+++ cleanup/main.py
@@ -0,0 +1,20 @@
+from config import Config
+from datetime import datetime, timedelta, timezone
+from time import sleep
+import pymongo
+
+
+# function for removing all queue rows with expired queue_finish time
+def remove_expired_queues():
+	client = pymongo.MongoClient(Config.MONGO_DSN)
+	conn = client[Config.DB_NAME]
+	conn = conn['queue']
+
+	conn.delete_many({'queue_finish': {'$lt': datetime.timestamp(datetime.utcnow().replace(tzinfo=timezone.utc))}})
+
+
+if __name__ == '__main__':
+	while True:
+		sleep(Config.CLEANUP_SLEEP)
+		remove_expired_queues()
+		print(datetime.utcnow(), '- Cleanup completed')
